a126fce83237871fb68fe5013c3633fd1394832c
SageWorks4925 <worksofsage@gmail.com>
2024-04-07 14:25:14 +0530

    dfgbn

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if 'transcripts' not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"]
+os.environ['OPENAI_API_KEY'] = st.secrets["openai_key"]
 
 print("AI")
 print(os.getenv('OPENAI_API_KEY'))
